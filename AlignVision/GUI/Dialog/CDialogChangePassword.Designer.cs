a6c2dfdb432f10f96f58b9c5dd9f725dcb972efa
nguyenPC <1535nguyen@gmail.com>
2026-05-08 00:19:42 +0700

    add dialog

---
+++ AlignVision/GUI/Dialog/CDialogChangePassword.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace AlignVision
+{
+    partial class CDialogChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTitlePassword = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.TextPassword = new System.Windows.Forms.TextBox();
+            this.checkBoxHidePassword = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnTitlePassword
+            // 
+            this.btnTitlePassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTitlePassword.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.btnTitlePassword.Location = new System.Drawing.Point(8, 10);
+            this.btnTitlePassword.Name = "btnTitlePassword";
+            this.btnTitlePassword.Size = new System.Drawing.Size(98, 28);
+            this.btnTitlePassword.TabIndex = 15;
+            this.btnTitlePassword.Text = "PASSWORD";
+            this.btnTitlePassword.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(164, 69);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 69);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(8, 69);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(150, 69);
+            this.btnOK.TabIndex = 14;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // TextPassword
+            // 
+            this.TextPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold);
+            this.TextPassword.Location = new System.Drawing.Point(112, 12);
+            this.TextPassword.Name = "TextPassword";
+            this.TextPassword.Size = new System.Drawing.Size(202, 26);
+            this.TextPassword.TabIndex = 12;
+            // 
+            // checkBoxHidePassword
+            // 
+            this.checkBoxHidePassword.AutoSize = true;
+            this.checkBoxHidePassword.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxHidePassword.Location = new System.Drawing.Point(169, 44);
+            this.checkBoxHidePassword.Name = "checkBoxHidePassword";
+            this.checkBoxHidePassword.Size = new System.Drawing.Size(145, 23);
+            this.checkBoxHidePassword.TabIndex = 18;
+            this.checkBoxHidePassword.Text = "Hide Password";
+            this.checkBoxHidePassword.UseVisualStyleBackColor = true;
+            // 
+            // CDialogChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 146);
+            this.ControlBox = false;
+            this.Controls.Add(this.checkBoxHidePassword);
+            this.Controls.Add(this.btnTitlePassword);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.TextPassword);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CDialogChangePassword";
+            this.ShowIcon = false;
+            this.Text = "Change Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTitlePassword;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox TextPassword;
+        private System.Windows.Forms.CheckBox checkBoxHidePassword;
+    }
+}
---
+++ AlignVision/GUI/Dialog/CDialogLogin.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace AlignVision
+{
+    partial class CDialogLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTitleLogin = new System.Windows.Forms.Button();
+            this.btnMaster = new System.Windows.Forms.Button();
+            this.btnEngineer = new System.Windows.Forms.Button();
+            this.btnTitlePassword = new System.Windows.Forms.Button();
+            this.btnOperator = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.checkBoxHidePassword = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnTitleLogin
+            // 
+            this.btnTitleLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTitleLogin.Font = new System.Drawing.Font("Consolas", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTitleLogin.Location = new System.Drawing.Point(5, 4);
+            this.btnTitleLogin.Name = "btnTitleLogin";
+            this.btnTitleLogin.Size = new System.Drawing.Size(306, 105);
+            this.btnTitleLogin.TabIndex = 10;
+            this.btnTitleLogin.Text = "LOGIN";
+            this.btnTitleLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnMaster
+            // 
+            this.btnMaster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaster.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaster.Location = new System.Drawing.Point(213, 115);
+            this.btnMaster.Name = "btnMaster";
+            this.btnMaster.Size = new System.Drawing.Size(98, 61);
+            this.btnMaster.TabIndex = 11;
+            this.btnMaster.Text = "MASTER";
+            this.btnMaster.UseVisualStyleBackColor = true;
+            // 
+            // btnEngineer
+            // 
+            this.btnEngineer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEngineer.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEngineer.Location = new System.Drawing.Point(109, 115);
+            this.btnEngineer.Name = "btnEngineer";
+            this.btnEngineer.Size = new System.Drawing.Size(98, 61);
+            this.btnEngineer.TabIndex = 12;
+            this.btnEngineer.Text = "ENGINEER";
+            this.btnEngineer.UseVisualStyleBackColor = true;
+            // 
+            // btnTitlePassword
+            // 
+            this.btnTitlePassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTitlePassword.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
+            this.btnTitlePassword.Location = new System.Drawing.Point(5, 181);
+            this.btnTitlePassword.Name = "btnTitlePassword";
+            this.btnTitlePassword.Size = new System.Drawing.Size(85, 26);
+            this.btnTitlePassword.TabIndex = 13;
+            this.btnTitlePassword.Text = "PASSWORD";
+            this.btnTitlePassword.UseVisualStyleBackColor = true;
+            // 
+            // btnOperator
+            // 
+            this.btnOperator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOperator.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOperator.Location = new System.Drawing.Point(5, 115);
+            this.btnOperator.Name = "btnOperator";
+            this.btnOperator.Size = new System.Drawing.Size(98, 61);
+            this.btnOperator.TabIndex = 14;
+            this.btnOperator.Text = "OPERATOR";
+            this.btnOperator.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(5, 244);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(152, 83);
+            this.btnLogin.TabIndex = 15;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold);
+            this.TxtPassword.Location = new System.Drawing.Point(96, 181);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(215, 26);
+            this.TxtPassword.TabIndex = 9;
+            this.TxtPassword.Text = "mat khau";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(163, 244);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(148, 83);
+            this.btnLogout.TabIndex = 16;
+            this.btnLogout.Text = "LOGOUT";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Visible = false;
+            // 
+            // checkBoxHidePassword
+            // 
+            this.checkBoxHidePassword.AutoSize = true;
+            this.checkBoxHidePassword.Location = new System.Drawing.Point(166, 215);
+            this.checkBoxHidePassword.Name = "checkBoxHidePassword";
+            this.checkBoxHidePassword.Size = new System.Drawing.Size(145, 23);
+            this.checkBoxHidePassword.TabIndex = 17;
+            this.checkBoxHidePassword.Text = "Hide Password";
+            this.checkBoxHidePassword.UseVisualStyleBackColor = true;
+            // 
+            // CDialogLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(317, 334);
+            this.ControlBox = false;
+            this.Controls.Add(this.checkBoxHidePassword);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnTitleLogin);
+            this.Controls.Add(this.btnMaster);
+            this.Controls.Add(this.btnEngineer);
+            this.Controls.Add(this.btnTitlePassword);
+            this.Controls.Add(this.btnOperator);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CDialogLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTitleLogin;
+        private System.Windows.Forms.Button btnMaster;
+        private System.Windows.Forms.Button btnEngineer;
+        private System.Windows.Forms.Button btnTitlePassword;
+        private System.Windows.Forms.Button btnOperator;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.CheckBox checkBoxHidePassword;
+    }
+}
